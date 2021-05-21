e2b76c2c516773522fc515a30ff8b542b04334df
AllanTori <allant22@gmail.com>
2021-05-21 19:48:46 -0300

    Estiliza a tela classificar (incompleto)

--- src/pages/Classificar/index.js
+++ src/pages/Classificar/index.js
@@ -8,9 +8,31 @@ export default function Classificar({ route, navigation }) {
 
   return (
     <View style={styles.container1}>
+      <Text style={styles.header}>Classificar a imagem:</Text>
       <View style={styles.imageContainer}>
         <Image source={image} style={styles.image} />
       </View>
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={styles.button} onPress={console.warn("Positivo")}>
+          <Text style={styles.buttonText}>Positivo</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={console.warn("Negativo")}>
+          <Text style={styles.buttonText}>Negativo</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={console.warn("Label 3")}>
+          <Text style={styles.buttonText}>Label 3</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={console.warn("Label 4")}>
+          <Text style={styles.buttonText}>Label 4</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.button2} onPress={console.warn("Salvo!")}>
+        <Text style={styles.buttonText}>Salvar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- src/pages/Classificar/styles.js
+++ src/pages/Classificar/styles.js
@@ -1,38 +1,58 @@
 import {Dimensions, StyleSheet} from 'react-native';
 
-const centralButton = {
-  width: 160,
-  height: 100,
-  borderRadius: 20,
-  position: 'absolute',
-  left: '50%',
-  marginLeft: -80,
-  justifyContent: 'center',
-  alignItems: 'center',
-};
-
 const styles = StyleSheet.create({
   container1: {
     flex: 1,
     backgroundColor: '#E9F5F5',
     alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  header: {
+    marginTop: 20,
+    width: Dimensions.get('window').width,
+    fontSize: 24,
+    color: '#363636',
+    fontWeight: 'bold',
+    alignItems: 'flex-start',
   },
   imageContainer: {
     width: '90%',
     height: (Dimensions.get('window').width * 3) / 4,
     backgroundColor: '#E9F5F5',
     marginTop: 20,
+    justifyContent: 'center',
   },
   image: {
     width: Dimensions.get('window').width,
     height: (Dimensions.get('window').width * 3) / 4,
     resizeMode: 'center',
   },
-  header: {
-    margin: 30,
-    marginBottom: 100,
-    fontSize: 28,
+  button: {
+    borderRadius: 20,
+    width: 200,
+    height: 40,
+    backgroundColor: '#2F4D90',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button2: {
+    borderRadius: 20,
+    width: 300,
+    height: 50,
+    backgroundColor: '#2F4D90',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 16,
     fontWeight: 'bold',
+  },
+  buttonsContainer: {
+    flex: 1,
+    backgroundColor: '#E9F5F5',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
 });
 
