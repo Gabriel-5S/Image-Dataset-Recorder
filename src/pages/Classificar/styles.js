a212418c40ee07ed1c9a00d0af00dccc8b10d47f
AllanTori <allant22@gmail.com>
2021-06-17 15:35:18 -0300

    Remove as dimensoes fixas do imageContainer.

--- src/pages/Classificar/styles.js
+++ src/pages/Classificar/styles.js
@@ -49,9 +49,6 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 1,
-    width: 250,
-    // height: (Dimensions.get('window').width * 3) / 4,
-    height: 250,
     marginTop: 80,
     // padding: 15,
     backgroundColor: '#E9F5F5',
